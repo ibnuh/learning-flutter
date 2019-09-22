84d6d77fb5476395c65ca7c49f0a640de7f54e8e
Muhammad Ibnuh <ibnuhdev@gmail.com>
2019-09-22 23:13:02 +0700

    Add random email generator

--- gmail-ui/lib/main.dart
+++ gmail-ui/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:faker/faker.dart';
+import 'dart:math';
 
 void main() => runApp(GmailApp());
 
@@ -20,26 +21,65 @@ class GmailApp extends StatelessWidget {
             ],
           ),
           drawer: Drawer(),
-          body: _buildEmails(),
+          body: RandomEmail(),
         ));
   }
+}
 
-  Widget _buildEmails() {
+class Email {
+  String sender;
+  String excerpt;
+}
+
+class RandomEmailsState extends State<RandomEmail> {
+  final List<Email> _emails = List<Email>();
+
+  List<Widget> _generateRandomEmails() {
+    for (var i = 0; i < 10; i++) {
+      var email = new Email();
+
+      email.sender = faker.person.name();
+      email.excerpt = faker.lorem.sentence();
+
+      _emails.add(email);
+    }
+
+    return _emails
+        .map((e) => Column(
+              children: <Widget>[
+                ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Colors
+                        .primaries[Random().nextInt(Colors.primaries.length)],
+                    child: Text(
+                      e.sender[0],
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                  title: Container(
+                    margin: const EdgeInsets.only(bottom: 5),
+                    child: Text(
+                      e.sender,
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  ),
+                  subtitle: Text(e.excerpt),
+                ),
+                Divider()
+              ],
+            ))
+        .toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return ListView(
-      children: <Widget>[
-        ListTile(
-          leading: CircleAvatar(
-            backgroundColor: Colors.green.shade300,
-            child: Text(
-              "A",
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-          title: Text("Amazon"),
-          subtitle: Text("Your order is in process mate, please be patient."),
-        ),
-        Divider(),
-      ],
+      children: _generateRandomEmails(),
     );
   }
+}
+
+class RandomEmail extends StatefulWidget {
+  @override
+  RandomEmailsState createState() => RandomEmailsState();
 }
